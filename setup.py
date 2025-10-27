a56c50ad4cd063c97f3eeb1137dda9dc4cddda4b
John <43251090+uhwang@users.noreply.github.com>
2025-10-27 12:47:48 -0400

    Setup for streamlines.pyx

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy
+
+setup(
+    name="streamlines",
+    ext_modules=cythonize(
+        "streamlines.pyx",
+        compiler_directives={"language_level": "3"},
+    ),
+    include_dirs=[numpy.get_include()],
+)
